ea693933a2f687c2448793156ca844edea6e887f
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-01 11:27:50 +0545

    made the network.py class

---
+++ network.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class YourModelName(nn.Module):
+    def __init__(self):
+        super(YourModelName, self).__init__()
+        #define your layers and activation function
+    
+    def forward(self, x):
+        #pass the input "x" through the layers we initialized in the init
+        pass
+
